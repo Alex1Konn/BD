72abf5bf6085d928a7ccc24b770b1ed60783dd10
Alex1Konn <n1kolaevna2019@yandex.ru>
2026-05-27 21:02:54 +0400

    Postgres1-3

--- main.py
+++ main.py
@@ -272,3 +272,148 @@ VALUES
 (2, 344, 'работник', 10000),
 (3, 343, 'работник', 10000)
 """
+
+SELECT * FROM products вместо * можем указываь столбцы которые хотим вывести
+
+SELECT
+	product_id,
+	product_name,
+	unit_price
+FROM products
+
+SELECT 2 * 2 + 100
+
+
+SELECT product_id, product_name, unit_price * units_in_stock AS "Итог"
+FROM products
+
+SELECT title
+FROM employees
+
+
+DISTINCT - убратьдубльВ ОДНОМ СТОДБЦЕ ИЛИ ТАБЛИЦЕ
+SELECT DISTINCT country
+FROM employees
+
+
+SELECT COUNT(*)
+FROM employees ПОДСЧИТАТЬ СТРОКИ
+
+SELECT COUNT(DISTINCT country)
+FROM employees  ПОДСЧИТАТЬ СТРОКИ С УНКИАЛЬНЫМИ ЗНАЧЕНИЯМИ
+
+SELECT * FROM customers
+
+SELECT contact_name, city
+FROM customers
+
+поиск с условиями
+SELECT company_name, contact_name, phone, country
+FROM customers
+WHERE country = 'USA'
+
+
+SELECT * FROM products
+WHERE unit_price > 20
+
+SELECT * FROM products
+WHERE discontinued = 1
+
+
+SELECT * FROM customers
+WHERE city <> 'Berlin'
+
+SELECT * FROM customers
+WHERE city != 'Berlin'
+
+SELECT * FROM orders
+WHERE order_date >= '1998-03-01'
+
+<>  знак неравно
+
+SELECT * FROM products
+WHERE unit_price > 25 AND units_in_stock > 40
+
+SELECT * FROM customers
+WHERE city = 'Berlin' OR city ='London' OR city='San Francisco'
+
+SELECT * FROM orders
+WHERE shipped_date > '1998-04-30' AND (freight < 75 OR freight > 150)
+
+
+SELECT * FROM orders
+WHERE freight >= 20 AND freight <= 40
+или АНАЛОГ ЗАПИСИ
+SELECT * FROM orders
+WHERE freight BETWEEN 20 AND 40
+
+SELECT COUNT(*) FROM orders
+WHERE freight BETWEEN 20 AND 40
+
+SELECT * FROM orders
+WHERE order_date BETWEEN '1998-03-30' AND '1998-04-03'
+
+SELECT * FROM orders
+WHERE order_date > '1998-03-30' AND order_date > '1998-04-03'
+
+SELECT * FROM customers
+WHERE country ='Mexico' OR country = 'Germany' OR country ='Canada'
+ИЛИ АЛЬТЕРНАТИВНАЯ ЗАПИСЬ
+SELECT * FROM customers
+WHERE country IN ('Mexico', 'Germany', 'Canada')
+
+
+SELECT * FROM products
+WHERE category_id IN (1, 3, 5, 7)
+
+SELECT * FROM customers
+WHERE country NOT IN ('Mexico', 'Germany', 'Canada')
+
+SELECT * FROM products
+WHERE category_id NOT IN (1, 3, 5, 7)
+
+SELECT DISTINCT country
+FROM customers
+ORDER BY country дает сортировку по алфавиту
+
+SELECT DISTINCT country
+FROM customers
+ORDER BY country ASC
+
+SELECT DISTINCT country
+FROM customers
+ORDER BY country DESC  в обратном алфаиту
+
+SELECT DISTINCT country, city (очиститьсЯ стобец указанным последним)
+FROM customers
+ORDER BY country DESC
+
+SELECT country, city
+FROM customers
+ORDER BY country DESC, city DESC
+
+SELECT country, city
+FROM customers
+ORDER BY country DESC, city DESC
+
+SELECT ship_city, order_date
+FROM orders
+WHERE ship_city = 'London'
+ORDER BY order_date
+
+SELECT MIN(order_date)
+FROM orders
+WHERE ship_city = 'London'
+
+SELECT MAX(order_date)
+FROM orders
+WHERE ship_city = 'London'
+
+SELECT * FROM orders
+WHERE ship_country IN ('France', 'Austria', 'Spain')
+
+SELECT * FROM orders
+ORDER BY  required_date DESC, shipped_date ASC
+
+SELECT MIN(unit_price) FROM products
+WHERE units_in_stock > 30
